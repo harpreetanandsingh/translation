f890b3e14de084623d12fe583734b2b462d72116
harpreetanandsingh <92373075+harpreetanandsingh@users.noreply.github.com>
2023-11-05 02:52:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,25 @@ from googletrans import Translator
 from gtts import gTTS
 from playsound import playsound
 from PIL import Image
+# import logging
+# import logging.handlers
+# import queue
+# import threading
+# import time
+# import urllib.request
+# import os
+# from collections import deque
+# from pathlib import Path
+# from typing import List
 
+# import av
+# import numpy as np
+# import pydub
+# import streamlit as st
+# from twilio.rest import Client
+
+# from streamlit_webrtc import WebRtcMode, webrtc_streamer
+from streamlit_mic_recorder import mic_recorder,speech_to_text
 # Initialize the recognizer
 recognizer = sr.Recognizer()
 
@@ -171,11 +189,41 @@ hide_st_style = """
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+# @st.cache_data  # type: ignore
+# def get_ice_servers():
+#     """Use Twilio's TURN server because Streamlit Community Cloud has changed
+#     its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
+#     We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
+#     but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
+#     See https://github.com/whitphx/streamlit-webrtc/issues/1213
+#     """
+
+#     # Ref: https://www.twilio.com/docs/stun-turn/api
+#     try:
+#         account_sid = "ACbea2776671d07a28bfa473b522b609fb"
+#         auth_token = "f9d226593c16124f8eaed4b8d2d5397b"
+#     except KeyError:
+#         logger.warning(
+#             "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
+#         )
+#         return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+#     client = Client(account_sid, auth_token)
+
+#     token = client.tokens.create()
+
+#     return token.ice_servers
 
 # Function to recognize speech
 def recognize_speech(prompt, language='en'):
     try:
-        with sr.Microphone() as source:
+        #with sr.Microphone() as source:
+        with mic_recorder(
+                    start_prompt="Start recording",
+                    stop_prompt="Stop recording", 
+                    just_once=False,
+                    use_container_width=False,
+                    key=None) as source:
             st.write(prompt)
             # Use recognizer to adjust to ambient noise
             recognizer.adjust_for_ambient_noise(source, duration=1)
